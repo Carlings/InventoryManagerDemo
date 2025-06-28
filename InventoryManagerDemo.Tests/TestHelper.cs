8cc76c8d825ec819858f57aa46a76bdaf6404b0b
Dmytro Zabrodskyi <dzabrodskiy03@gmail.com>
2025-06-28 13:13:46 +0300

    Clean up project structure, implement DbContext, and develop Product controller.

---
+++ InventoryManagerDemo.Tests/TestHelper.cs
@@ -0,0 +1,31 @@
+﻿using InventoryManagerDemo.Data.Models;
+using InventoryManagerDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagerDemo.Tests
+{
+    internal static class TestHelper
+    {
+        public static InventoryManagerContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<InventoryManagerContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new InventoryManagerContext(options);
+
+            context.Products.Add(new Product
+            {
+                Id = 1,
+                Name = "Test Product",
+                Description = "Test description",
+                Price = 99.99m,
+                Quantity = 10
+            });
+
+            context.SaveChanges();
+
+            return context;
+        }
+    }
+}
